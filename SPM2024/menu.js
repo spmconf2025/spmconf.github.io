a22f5703f133ae844a4247cf65a9db292286419a
Tsz Ho Kwok <tszho.kwok@concordia.ca>
2023-07-27 21:05:38 -0400

    menu

---
+++ SPM2024/menu.js
@@ -0,0 +1,15 @@
+document.write('\
+<div class="collapse navbar-collapse sub-menu-bar" id="navbarSupportedContent">\
+	<ul id="nav" class="navbar-nav ms-auto">\
+		<li class="nav-item"><a id="about" href="index.html" aria-label="Toggle navigation">About</a></li>\
+		<li class="nav-item"><a id="CfP" href="CfP.html" aria-label="Toggle navigation">Call for Papers</a></li>\
+		<li class="nav-item"><a id="venue" href="Venue.html" aria-label="Toggle navigation">Venue</a></li>\
+		<li class="nav-item"><a id="organizer" href="Organizer.html" aria-label="Toggle navigation">Organizers</a></li>\
+		<li class="nav-item"><a id="award" href="Award.html" aria-label="Toggle navigation">Bézier Award</a></li>\
+		<li class="nav-item"><a id="keynote" href="Keynote.html" aria-label="Toggle navigation">Keynotes</a></li>\
+		<li class="nav-item"><a id="program" href="Program.html" aria-label="Toggle navigation">Program</a></li>\
+		<li class="nav-item"><a id="sponsor" href="Sponsor.html" aria-label="Toggle navigation">Sponsor</a></li>\
+		<li class="nav-item"><a href="http://solidmodeling.org/spm-conferences/" target="new" aria-label="Toggle navigation">Previous Conferences</a></li>\
+	</ul>\
+</div>\
+');
