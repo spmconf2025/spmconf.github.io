143b8fc6e8469df120bfbf1e311eaef3d3fe92db
Tsz Ho Kwok <tszho.kwok@concordia.ca>
2023-08-02 12:15:30 -0400

    reorganized

--- SPM2024/menu.js
+++ SPM2024/menu.js
@@ -1,13 +1,29 @@
 document.write('\
 <div class="collapse navbar-collapse sub-menu-bar" id="navbarSupportedContent">\
 	<ul id="nav" class="navbar-nav ms-auto">\
-		<li class="nav-item"><a id="about" href="index.html" aria-label="Toggle navigation">About</a></li>\
-		<li class="nav-item"><a id="CfP" href="CfP.html" aria-label="Toggle navigation">Call for Papers</a></li>\
-		<li class="nav-item"><a id="venue" href="Venue.html" aria-label="Toggle navigation">Venue</a></li>\
-		<li class="nav-item"><a id="organizer" href="Organizer.html" aria-label="Toggle navigation">Organizers</a></li>\
+		<li class="nav-item">\
+			<a id="about" class="dd-menu collapsed" href="index.html" data-bs-target="#submenu-1-1" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">About</a>\
+			<ul class="sub-menu collapse" id="submenu-1-1">\
+				<li class="nav-item"><a href="index.html">Conference</a></li>\
+				<li class="nav-item"><a href="Organizer.html">Organizers</a></li>\
+			</ul>\
+		</li>\
+		<li class="nav-item">\
+			<a id="program" class="dd-menu collapsed" href="CfP.html" data-bs-target="#submenu-1-1" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">Program</a>\
+			<ul class="sub-menu collapse" id="submenu-1-1">\
+				<li class="nav-item"><a href="CfP.html">Call for Papers</a></li>\
+				<li class="nav-item"><a href="Program.html">Schedule</a></li>\
+			</ul>\
+		</li>\
+		<li class="nav-item">\
+			<a id="venue" class="dd-menu collapsed" href="Venue.html" data-bs-target="#submenu-1-1" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">Travel</a>\
+			<ul class="sub-menu collapse" id="submenu-1-1">\
+				<li class="nav-item"><a href="Venue.html">Venue</a></li>\
+				<li class="nav-item"><a href="Childcare.html">Childcare Services</a></li>\
+			</ul>\
+		</li>\
 		<li class="nav-item"><a id="award" href="Award.html" aria-label="Toggle navigation">Bézier Award</a></li>\
 		<li class="nav-item"><a id="keynote" href="Keynote.html" aria-label="Toggle navigation">Keynotes</a></li>\
-		<li class="nav-item"><a id="program" href="Program.html" aria-label="Toggle navigation">Program</a></li>\
 		<li class="nav-item"><a id="sponsor" href="Sponsor.html" aria-label="Toggle navigation">Sponsor</a></li>\
 		<li class="nav-item"><a href="http://solidmodeling.org/spm-conferences/" target="new" aria-label="Toggle navigation">Previous Conferences</a></li>\
 	</ul>\
