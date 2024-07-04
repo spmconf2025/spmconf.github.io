46fc68c5d86740f190a7a24cc999d58ef1123336
Tsz Ho Kwok <tszho.kwok@concordia.ca>
2024-07-04 11:10:31 -0400

    Update menu.js

--- SPM2024/menu.js
+++ SPM2024/menu.js
@@ -8,8 +8,24 @@ document.write('\
 				<li class="nav-item"><a href="Organizer.html">Organizers</a></li>\
 			</ul>\
 		</li>\
+		<li class="nav-item"><a id="program" href="Program.html" aria-label="Toggle navigation">Program</a></li>\
 		<li class="nav-item">\
-			<a id="program" class="dd-menu collapsed" href="CfP.html" data-bs-toggle="collapse" data-bs-target="#submenu-1-1" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">Program</a>\
+			<a id="venue" class="dd-menu collapsed" href="Venue.html" data-bs-toggle="collapse" data-bs-target="#submenu-1-1" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">Travel</a>\
+			<ul class="sub-menu collapse" id="submenu-1-1">\
+				<li class="nav-item"><a href="Venue.html">Venue</a></li>\
+				<li class="nav-item"><a href="Accommodation.html">Accommodation</a></li>\
+				<li class="nav-item"><a href="Local.html">Local Info</a></li>\
+			</ul>\
+		</li>\
+		<li class="nav-item"><a id="keynote" href="Keynote.html" aria-label="Toggle navigation">Keynotes</a></li>\
+		<li class="nav-item"><a href="http://solidmodeling.org/spm-conferences/" target="new" aria-label="Toggle navigation">Previous Conferences</a></li>\
+	</ul>\
+</div>\
+');
+
+/*
+		<li class="nav-item">\
+			<a id="program" class="dd-menu collapsed" href="Program.html" data-bs-toggle="collapse" data-bs-target="#submenu-1-1" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">Program</a>\
 			<ul class="sub-menu collapse" id="submenu-1-1">\
 				<li class="nav-item"><a href="CfP.html">Call for Papers</a></li>\
 				<li class="nav-item"><a href="Poster.html">Call for Posters</a></li>\
@@ -17,20 +33,9 @@ document.write('\
 				<li class="nav-item"><a href="Program.html">Schedule</a></li>\
 			</ul>\
 		</li>\
-		<li class="nav-item">\
-			<a id="venue" class="dd-menu collapsed" href="Venue.html" data-bs-toggle="collapse" data-bs-target="#submenu-1-1" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">Travel</a>\
-			<ul class="sub-menu collapse" id="submenu-1-1">\
-				<li class="nav-item"><a href="Venue.html">Venue</a></li>\
-				<li class="nav-item"><a href="Accommodation.html">Accommodation</a></li>\
+		<li class="nav-item"><a id="award" href="Award.html" aria-label="Toggle navigation">Bézier Award</a></li>\
+		<li class="nav-item"><a id="sponsor" href="Sponsor.html" aria-label="Toggle navigation">Sponsor</a></li>\
+		
 				<li class="nav-item"><a href="Childcare.html">Childcare Services</a></li>\
 				<li class="nav-item"><a href="VisitCanada.html">Visit Canada</a></li>\
-				<li class="nav-item"><a href="Local.html">Local Info</a></li>\
-			</ul>\
-		</li>\
-		<!--<li class="nav-item"><a id="award" href="Award.html" aria-label="Toggle navigation">Bézier Award</a></li>-->\
-		<li class="nav-item"><a id="keynote" href="Keynote.html" aria-label="Toggle navigation">Keynotes</a></li>\
-		<li class="nav-item"><a id="sponsor" href="Sponsor.html" aria-label="Toggle navigation">Sponsor</a></li>\
-		<li class="nav-item"><a href="http://solidmodeling.org/spm-conferences/" target="new" aria-label="Toggle navigation">Previous Conferences</a></li>\
-	</ul>\
-</div>\
-');
+*/
