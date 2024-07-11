d6c9188078aad61eb05e059f4143cbebd6dff6d3
Tsz Ho Kwok <tszho.kwok@concordia.ca>
2024-07-10 20:40:05 -0400

    Update menu.js

--- SPM2024/menu.js
+++ SPM2024/menu.js
@@ -8,7 +8,15 @@ document.write('\
 				<li class="nav-item"><a href="Organizer.html">Organizers</a></li>\
 			</ul>\
 		</li>\
-		<li class="nav-item"><a id="program" href="Program.html" aria-label="Toggle navigation">Program</a></li>\
+		<li class="nav-item">\
+			<a id="program" class="dd-menu collapsed" href="Program.html" data-bs-toggle="collapse" data-bs-target="#submenu-1-1" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">Program</a>\
+			<ul class="sub-menu collapse" id="submenu-1-1">\
+				<li class="nav-item"><a href="CfP.html">Call for Papers</a></li>\
+				<li class="nav-item"><a href="Poster.html">Call for Posters</a></li>\
+				<li class="nav-item"><a href="CAD.html">Presentation-only Papers</a></li>\
+				<li class="nav-item"><a href="Program.html">Schedule</a></li>\
+			</ul>\
+		</li>\
 		<li class="nav-item">\
 			<a id="venue" class="dd-menu collapsed" href="Venue.html" data-bs-toggle="collapse" data-bs-target="#submenu-1-1" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">Travel</a>\
 			<ul class="sub-menu collapse" id="submenu-1-1">\
@@ -24,15 +32,7 @@ document.write('\
 ');
 
 /*
-		<li class="nav-item">\
-			<a id="program" class="dd-menu collapsed" href="Program.html" data-bs-toggle="collapse" data-bs-target="#submenu-1-1" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">Program</a>\
-			<ul class="sub-menu collapse" id="submenu-1-1">\
-				<li class="nav-item"><a href="CfP.html">Call for Papers</a></li>\
-				<li class="nav-item"><a href="Poster.html">Call for Posters</a></li>\
-				<li class="nav-item"><a href="CAD.html">Presentation-only Papers</a></li>\
-				<li class="nav-item"><a href="Program.html">Schedule</a></li>\
-			</ul>\
-		</li>\
+		<li class="nav-item"><a id="program" href="Program.html" aria-label="Toggle navigation">Program</a></li>\
 		<li class="nav-item"><a id="award" href="Award.html" aria-label="Toggle navigation">Bézier Award</a></li>\
 		<li class="nav-item"><a id="sponsor" href="Sponsor.html" aria-label="Toggle navigation">Sponsor</a></li>\
 		
